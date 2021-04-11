715cff3e357a2185a7b6137d926a44f9af21fe76
spoonik88 <spoonik@tut.by>
2021-04-11 16:48:10 +0300

    refactoring code

--- main.js
+++ main.js
@@ -20,11 +20,9 @@ const Player2 = {
     },
 };
 
-
 const $playerLeft = document.querySelector(".arenas");
 
 const btnHtmlCreate = () => {
-
     $playerLeft.insertAdjacentHTML(
         "afterbegin",
         `
@@ -35,38 +33,30 @@ const btnHtmlCreate = () => {
 </div>
 `
     );
-}
+};
 btnHtmlCreate();
 
-
-
-
 const createPlayer = (playerNumber, player) => {
-
-
-    const $player = document.createElement('div');
-    const $progressbar = document.createElement('div');
-    const $playerLife = document.createElement('div');
-    const $playerName = document.createElement('div');
-    const $character = document.createElement('div');
-    const $playerImg = document.createElement('img');
+    const $player = document.createElement("div");
+    const $progressbar = document.createElement("div");
+    const $playerLife = document.createElement("div");
+    const $playerName = document.createElement("div");
+    const $character = document.createElement("div");
+    const $playerImg = document.createElement("img");
 
     $player.classList.add(playerNumber);
-    $progressbar.classList.add('progressbar');
+    $progressbar.classList.add("progressbar");
 
-    $playerLife.classList.add('life');
-    $playerLife.setAttribute('value', player.hp);
+    $playerLife.classList.add("life");
+    $playerLife.style.width = player.hp + "%";
+    $playerLife.setAttribute("value", player.hp);
 
-
-
-    $playerName.classList.add('name');
+    $playerName.classList.add("name");
     $playerName.innerText = player.name;
 
-    $character.classList.add('character');
+    $character.classList.add("character");
 
     $playerImg.src = player.img;
-
-
 
     $playerLeft.appendChild($player);
     $player.appendChild($progressbar);
@@ -74,55 +64,71 @@ const createPlayer = (playerNumber, player) => {
     $progressbar.appendChild($playerLife);
     $progressbar.appendChild($playerName);
     $character.appendChild($playerImg);
-
-}
-createPlayer('player1', Player1);
-createPlayer('player2', Player2);
-
+};
+createPlayer("player1", Player1);
+createPlayer("player2", Player2);
 
 const btnCame = document.querySelector(".button");
 
+const playerFiate = (player) => {
+    const $playerHP = document.querySelector('.player' + player.player + ' .life')
 
-console.log(Player2.hp)
-btnCame.addEventListener('click', function() {
-
-    const $player1HP = document.querySelectorAll(".life")[0]
-    const $player2HP = document.querySelectorAll(".life")[1]
-
-
-    Player1.hp = Player1.hp - Math.ceil(Math.random() * 20)
-    if (Player1.hp <= 0) {
-        console.log("ghbdtn");
-        Player1.hp = 0
-        $player1HP.style.width = Player1.hp + "%";
+    player.hp = player.hp - Math.ceil(Math.random() * 20);
+    if (player.hp <= 0) {
+        player.hp = 0;
+        $playerHP.style.width = player.hp + "%";
     } else {
-        $player1HP.style.width = Player1.hp + "%";
+        $playerHP.style.width = player.hp + "%";
     }
-
-    Player2.hp = Player2.hp - Math.ceil(Math.random() * 20)
-    if (Player2.hp <= 0) {
-        Player2.hp = 0
-        $player2HP.style.width = Player2.hp + "%";
-    } else {
-        $player2HP.style.width = Player2.hp + "%";
-    }
+}
 
 
-    console.log(Player2.hp, Player1.hp);
+
+
+
+btnCame.addEventListener("click", function() {
+    const $player1HP = document.querySelectorAll(".life")[0];
+    const $player2HP = document.querySelectorAll(".life")[1];
+
+    // Player1.hp = Player1.hp - Math.ceil(Math.random() * 20);
+    // if (Player1.hp <= 0) {
+    //     Player1.hp = 0;
+    //     $player1HP.style.width = Player1.hp + "%";
+    // } else {
+    //     $player1HP.style.width = Player1.hp + "%";
+    // }
+
+    // Player2.hp = Player2.hp - Math.ceil(Math.random() * 20);
+    // if (Player2.hp <= 0) {
+    //     Player2.hp = 0;
+    //     $player2HP.style.width = Player2.hp + "%";
+    // } else {
+    //     $player2HP.style.width = Player2.hp + "%";
+    // }
+
+    // console.log(Player2.hp, Player1.hp);
+    playerFiate(Player2)
+    playerFiate(Player1)
+
     if (Player2.hp == 0 || Player1.hp == 0) {
-        btnCame.disabled = true
+        btnCame.disabled = true;
+
+        const $createWins = document.createElement("h1");
+        const $playerWins = document.querySelector(".control");
+
+        $createWins.setAttribute(
+            "style",
+            " position: absolute;  color: #fff; font-family: 'Mortal Kombat 3';text-align: center;   width: 205px;"
+        );
+
+        $playerWins.appendChild($createWins);
 
         if (Player2.hp > Player1.hp) {
-            console.log(Player2.name + " " + "win")
-
+            $createWins.innerText = Player2.name + " " + "wins";
         } else if (Player2.hp === Player1.hp) {
-            console.log("draw")
+            $createWins.innerText = "draw";
         } else {
-            console.log(Player1.name + " " + "win")
+            $createWins.innerText = Player1.name + " " + "wins";
         }
-
     }
-
-
-
-})
+});
